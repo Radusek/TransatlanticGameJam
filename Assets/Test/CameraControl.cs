10317504b3925bfa4854be63766b9293b3a524d2
Radosław Rychlik <radusek4@gmail.com>
2020-07-13 00:41:52 +0200

    Cinemachine added

---
+++ Assets/Test/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraControl : MonoBehaviour
+{
+    public CinemachineVirtualCamera cam1;
+    public CinemachineVirtualCamera cam2;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int tmp = cam1.Priority;
+            cam1.Priority = cam2.Priority;
+            cam2.Priority = tmp;
+        }
+    }
+}
---
+++ Assets/Test/CubeMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    public float speed = 1f;
+
+
+    void Update()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(xMove, 0f, zMove).normalized * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
