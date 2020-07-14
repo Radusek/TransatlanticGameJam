ea6a7f5a32c83fdeeafbf4b099066a4ed27e417b
Radosław Rychlik <radusek4@gmail.com>
2020-07-14 21:19:58 +0200

    backup

--- Assets/_Project/Camera/Scripts/CameraOrbiting.cs
+++ Assets/_Project/Camera/Scripts/CameraOrbiting.cs
@@ -5,18 +5,20 @@ public class CameraOrbiting : MonoBehaviour
 {
     [SerializeField]
     private float sensitivity = 1f;
+
     [SerializeField]
     private Vector2 orbitRange;
+    private float currentOrbit;
+
     [SerializeField]
     private int zoomLevels = 3;
-
+    private int MaxZoomLevel => zoomLevels - 1;
     private int currentZoomLevel;
-    private float currentOrbit;
 
 
     private void Awake()
     {
-        SetZoomLevel(zoomLevels - 1);
+        SetZoomLevel(MaxZoomLevel);
     }
 
     void Update()
@@ -36,12 +38,17 @@ public class CameraOrbiting : MonoBehaviour
         Vector3 mouseDeltaPosition = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
         Vector3 rotation = transform.rotation.eulerAngles;
         rotation += mouseDeltaPosition * sensitivity;
+        rotation = ClampEulerRotationX(rotation);
+        transform.eulerAngles = rotation;
+    }
+
+    private static Vector3 ClampEulerRotationX(Vector3 rotation)
+    {
         if (rotation.x < 180 && rotation.x > 85)
             rotation.x = 85;
         if (rotation.x >= 180 && rotation.x < 275)
             rotation.x = 275;
-
-        transform.eulerAngles = rotation;
+        return rotation;
     }
 
     private void SetCameraPosition()
@@ -51,14 +58,13 @@ public class CameraOrbiting : MonoBehaviour
 
     private void ChangeOrbit(float deltaScroll)
     {
-        if (deltaScroll < 0 && currentZoomLevel > 0 || deltaScroll > 0 && currentZoomLevel < zoomLevels - 1)
+        if (deltaScroll < 0 && currentZoomLevel > 0 || deltaScroll > 0 && currentZoomLevel < MaxZoomLevel)
             SetZoomLevel(currentZoomLevel + (int)Mathf.Sign(deltaScroll));
     }
 
     private void SetZoomLevel(int level)
     {
-        float t = (float)level / (zoomLevels - 1);
-        
+        float t = (float)level / MaxZoomLevel;
         float endValue = Mathf.Lerp(orbitRange.x, orbitRange.y, t);
         DOTween.To(() => currentOrbit, x => currentOrbit = x, endValue, 0.4f);
         currentZoomLevel = level;
---
+++ Assets/_Project/Camera/Scripts/Raycaster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+    private Camera camera;
+
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            ShootRaycast();
+    }
+
+    private void ShootRaycast()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            Debug.Log($"{hit.point.magnitude} {hit.transform.gameObject.layer}");
+    }
+}
